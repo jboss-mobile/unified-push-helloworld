5db48eedcd1e4bc2eebdde3c0d46d1c891b9b1b4
Daniel Passos <daniel@passos.me>
2014-04-09 15:54:36 -0300

    [Android]: Display message in the list

--- android/src/org/jboss/aerogear/unifiedpush/helloworld/activities/MessagesActivity.java
+++ android/src/org/jboss/aerogear/unifiedpush/helloworld/activities/MessagesActivity.java
@@ -3,16 +3,27 @@ package org.jboss.aerogear.unifiedpush.helloworld.activities;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import org.jboss.aerogear.android.unifiedpush.MessageHandler;
 import org.jboss.aerogear.android.unifiedpush.Registrations;
+import org.jboss.aerogear.unifiedpush.helloworld.R;
 import org.jboss.aerogear.unifiedpush.helloworld.handler.NotificationBarMessageHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessagesActivity extends Activity implements MessageHandler{
+
+    private ListView listView;
+    private List<String> messages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.messages);
+
+        listView = (ListView) findViewById(R.id.messages);
     }
 
     @Override
@@ -31,16 +42,23 @@ public class MessagesActivity extends Activity implements MessageHandler{
 
     @Override
     public void onMessage(Context context, Bundle message) {
-        Toast.makeText(context, message.getString("alert"), Toast.LENGTH_SHORT).show();
+        String alert = message.getString("alert");
+        addNewMessage(alert);
     }
 
     @Override
     public void onDeleteMessage(Context context, Bundle message) {
-
     }
 
     @Override
     public void onError() {
-
     }
+
+    private void addNewMessage(String alert) {
+        messages.add(alert);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.message_item, messages);
+        listView.setAdapter(adapter);
+    }
+
 }
