7b3267d5c54747827651a08f2c30d05be14f4c07
Daniel Passos <daniel@passos.me>
2014-04-09 13:25:00 -0300

    [Android]: Add timer on splash screen

--- android/src/org/jboss/aerogear/unifiedpush/helloworld/activities/SplashScreenActivity.java
+++ android/src/org/jboss/aerogear/unifiedpush/helloworld/activities/SplashScreenActivity.java
@@ -2,6 +2,8 @@ package org.jboss.aerogear.unifiedpush.helloworld.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 import org.jboss.aerogear.unifiedpush.helloworld.R;
 
 public class SplashScreenActivity extends Activity {
@@ -10,5 +12,13 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Time to start UPS registration", Toast.LENGTH_SHORT).show();
+            }
+        }, 3000);
     }
 }
